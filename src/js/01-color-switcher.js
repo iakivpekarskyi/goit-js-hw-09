f2185b25183b3b4e848bfe961a472406f722cd51
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2023-01-17 15:53:30 +0200

    changed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,21 +1,22 @@
-const startBtn = document.querySelector("[data-start]");
-const stopBtn = document.querySelector("[data-stop]");
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 let timerId = null;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-startBtn.addEventListener("click", () => {
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
+
+function onStart(event) {
   timerId = setInterval(() => {
     startBtn.disabled = true;
     document.body.style.background = getRandomHexColor();
   }, 1000);
-});
+}
 
-stopBtn.addEventListener("click", () => {
+function onStop(event) {
   clearInterval(timerId);
   startBtn.disabled = false;
-});
- 
-
+}
